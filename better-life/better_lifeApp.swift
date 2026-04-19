42ff71026e63631ec68044827163e8a78f3f6359
krisjczhang <krisjczhang@tencent.com>
2026-04-19 15:13:41 +0800

    Initial Commit

---
+++ better-life/better_lifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_lifeApp.swift
+//  better-life
+//
+//  Created by 张金琛 on 2026/4/19.
+//
+
+import SwiftUI
+
+@main
+struct better_lifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
